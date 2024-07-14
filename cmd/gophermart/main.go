5729d13757a0d23b0aab41268a0cd664409a9a37
Pavel <pinbrain@yandex.ru>
2024-07-14 12:11:52 +0300

    more logs

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/pinbrain/gophermart/internal/app"
+import (
+	"fmt"
+
+	"github.com/pinbrain/gophermart/internal/app"
+)
 
 func main() {
+	fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ starting app")
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
